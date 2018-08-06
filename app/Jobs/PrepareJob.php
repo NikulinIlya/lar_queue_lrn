bbe89910333f214f29e33aa07b7f35f6f79b8fd8
Ilya <ilia.nikulin@gmail.com>
2018-08-06 16:32:36 +0300

    failed jobs

--- app/Jobs/PrepareJob.php
+++ app/Jobs/PrepareJob.php
@@ -7,12 +7,13 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Exception;
 
 class PrepareJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $tries = 3;
+    public $tries = 1;
 
     protected $message;
 
@@ -30,9 +31,21 @@ class PrepareJob implements ShouldQueue
      * Execute the job.
      *
      * @return void
+     * @throws Exception
      */
     public function handle()
     {
+        throw new Exception("Our Error", 101);
         info($this->message);
+    }
+
+    /**
+     * The job failed to process
+     *
+     * @param Exception $exception
+     */
+    public function failed(Exception $exception)
+    {
+        info(__CLASS__.": ошибка выполнения");
     }
 }
